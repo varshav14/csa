e06439262270bad82c404f49c2f673b3b7d42045
varshav14 <91842861+varshav14@users.noreply.github.com>
2021-12-12 17:14:10 -0800

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,31 @@
+package sum;
+
+public class Multiply {
+
+
+		public static void main(String[] args) {
+			
+			System.out.println("Product of the first 10 integers is: ");
+			
+			int product = 1;
+			for (int i=1; i<=10; i++) {
+				
+				product = i * product;
+			}
+			
+			System.out.println("product is " + product);
+			
+			
+		
+				
+				
+				
+				
+				
+			}
+				
+
+		
+
+	}
+
---
+++ Sum.java
@@ -0,0 +1,30 @@
+package sum;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sum of first 10 integers is: ");
+		
+		int sum = 0;
+		for (int i=1; i<=10; i++) {
+			
+			sum = i + sum;
+		}
+		
+		System.out.println("Sum is " + sum);
+		
+		
+	
+			
+			
+			
+			
+			
+		}
+			
+
+	}
+
+
+
